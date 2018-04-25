3e8da718ef1800751d6c404d2c7ece4c0d3b1f77
patarapolw <patarapolw@gmail.com>
2018-04-25 12:29:24 +0700

    Add analysis of Entropy module

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
-setup(name='passwordstrength',
-      version='0.2.0',
-      description='Editable password strength calculator for Python',
-      author='Pacharapol Withayasakpunt',
-      author_email='patarapolw@gmail.com',
-      license='MIT',
-      keywords='password password_strength password_meter',
-      packages=['passwordstrength'],
-      )
+setup(
+    name='passwordstrength',
+    version='0.2.1',
+    description='Editable password strength calculator for Python',
+    author='Pacharapol Withayasakpunt',
+    author_email='patarapolw@gmail.com',
+    license='MIT',
+    keywords='password password_strength password_meter',
+    packages=['passwordstrength'],
+    package_data={
+        'passwordstrength': ['database/*']
+    },
+    include_package_data=True,
+    extras_require={
+        'analysis': ['randomsentence', 'pronounceable', 'memorable_password']
+    }
+)
